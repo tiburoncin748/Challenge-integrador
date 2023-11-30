aea5279a5f3a22e47229fca9b364f92d0383a6ed
tiburoncin748 <lauty.pombo@gmail.com>
2023-11-30 18:20:01 -0300

    [FEAT] Configurado de Routes y pruebas OK

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+router.get('/login', (req, res) => res.send('This is the LOGIN view'));
+router.post('/login', (req, res) => res.send('This is the LOGIN SUBMIT view'));
+router.get('/register', (req, res) => res.send('This is the REGISTER view'));
+router.post('/register', (req, res) => res.send('This is the para submitear un register view'));
+router.get('/logout', (req, res) => res.send('This is the LOGOUT view'));
+
+
+
+
 module.exports = router;
