da231e242b280835d7b3342a7164d1d458f3e721
tiburoncin748 <lauty.pombo@gmail.com>
2023-12-10 17:24:28 -0300

    [FEAT]agregado y pruebas de shop sin base de datos, por ahora funciona

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,23 +2,29 @@ const path = require('path');
 
 module.exports = {
     admin: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/pages/admin/admin.ejs'));
+        res.render(path.resolve(__dirname, '../views/admin/admin.ejs'), {
+            title: 'Admin',
+    });
     },
 
     create: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/pages/admin/create.ejs'))
+        res.render(path.resolve(__dirname, '../views/admin/create.ejs'), {
+            title: 'Crear Item',
+    })
     },
 
     createItem: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/pages/admin/create.ejs'))
+        res.render(path.resolve(__dirname, '../views/admin/create.ejs'))
     },
 
     edit: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/pages/admin/edit.ejs'))
+        res.render(path.resolve(__dirname, '../views/admin/edit.ejs'), {
+            title: 'Editar Item',
+    })
     },
 
     editItem: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/pages/admin/edit.ejs'))
+        res.render(path.resolve(__dirname, '../views/admin/edit.ejs'))
     },
 
     deleteItem: (req, res) => {
